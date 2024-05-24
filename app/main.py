9a8166cb61f141fe341126a571d695f66d2b5007
AngelinaShmit <89831023404@mail.ru>
2024-05-24 21:32:42 +0300

    new commit

--- app/main.py
+++ app/main.py
@@ -3,6 +3,11 @@ from app.config import settings
 from app.database import create_tables
 from app.router_users import router as users_router
 from datetime import datetime
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from starlette.requests import Request
+
 
 def start_application():
     app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
@@ -10,6 +15,12 @@ def start_application():
     return app    
 
 app = start_application()
+
+# Подключаем статические файлы
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Подключаем Jinja2Templates
+templates = Jinja2Templates(directory="templates")
 
 @app.on_event("startup")
 def on_startup():
@@ -25,3 +36,7 @@ def shutdown():
 @app.get("/")
 def main():
     return {"message": "Hello, World!"}
+
+@app.get("/", response_class=HTMLResponse)
+def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
